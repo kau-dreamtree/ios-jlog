da1efa42c87312d15618b28d9f3c70c9a1776438
JISU LEE <wltn102998@gmail.com>
2024-06-30 21:27:46 +0900

    [FIX] 입장 화면에서 코드 입력 시 버튼 문구가 안보이는 현상 해결

--- JLog/ViewControllers/EnteranceViewController.swift
+++ JLog/ViewControllers/EnteranceViewController.swift
@@ -32,8 +32,8 @@ final class EnteranceViewController: JLogBaseViewController {
     private let enter: UIButton = {
         let button = UIButton()
         button.setTitle(LocalizableStrings.localize("enter"), for: .normal)
+        button.setTitleColor(.buttonTitle, for: .normal)
         button.titleLabel?.font = .regularFont
-        button.titleLabel?.textColor = .tertiaryLabel
         button.backgroundColor = .buttonOff
         button.layer.cornerRadius = 10
         return button
